f27959cea96b225f2e229975647deb80018349cc
ecardosa <u6105608@campus.udg.edu>
2026-02-26 19:38:40 +0100

    canvi d'estil i implementació de jugar

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 addEventListener('load', function() {
     document.getElementById('play').addEventListener('click', 
     function(){
-        alert("Comença la partida");
+       const alias = prompt("Introdueix l'àlies: ");
+        console.log(alias);
     });
 
     document.getElementById('options').addEventListener('click', 
